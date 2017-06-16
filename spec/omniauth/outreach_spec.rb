20e24b2938b20913683ec89197d0cac294d6dc04
Dan Kozlowski <dan.kozlowski@outreach.io>
2017-06-15 17:28:44 -0700

    Clean up gem

--- spec/omniauth/outreach_spec.rb
+++ spec/omniauth/outreach_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Omniauth::Outreach do
   it "has a version number" do
     expect(Omniauth::Outreach::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
